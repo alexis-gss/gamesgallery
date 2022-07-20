52eb1e9be548944db60991a5b76c3a792f4289aa
Alexis Gousseau <alexis.gousseau@ldc.fr>
2022-07-20 14:33:56 +0200

    fix content of the navbar

--- resources/lang/en/navbar.php
+++ resources/lang/en/navbar.php
@@ -2,8 +2,6 @@
 
 return [
     'access_website' => 'Access to the website',
-    'site' => 'Site',
-    'role' => 'Role',
     'to_disconnect' => 'To disconnect',
     'disconnect' => 'Disconnect',
 ];
--- resources/views/layouts/backend/navbar.blade.php
+++ resources/views/layouts/backend/navbar.blade.php
@@ -2,11 +2,7 @@
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('homepage') }}" target="_blank"
             title="{{ __('navbar.access_website') }}">
-            @if (auth()->user()->site ?? false)
-                {{ __('navbar.site') }} : <b class="text-primary">{{ ucwords(auth()->user()->site->name) }}</b>
-            @else
-                {{ config('app.name', 'Laravel') }}
-            @endif
+            {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
             aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,10 +10,16 @@
         </button>
         @auth
             <div class="collapse navbar-collapse" id="navbarScroll">
-                <ul class="navbar-nav ms-auto my-2 my-lg-0 mt-0 mb-0 navbar-nav-scroll px-4"
+                <ul class="navbar-nav ms-auto my-2 my-lg-0 mt-0 mb-0 navbar-nav-scroll px-3"
                     style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <span class="nav-link text-info">{{ __('navbar.role') }} : {{ auth()->user()->role }}</span>
+                        <span class="nav-link text-info px-3">
+                            @if (auth()->user()->role)
+                                Administrator
+                            @else
+                                User
+                            @endif
+                        </span>
                     </li>
                     <li class="nav-item">
                         <span class="nav-link text-success px-0">{{ auth()->user()->name }}</span>
