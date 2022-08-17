eb7e290df11c3464df9eceb2fd19f6d6f208b611
Alexis Gousseau <alexis.gousseau@ldc.fr>
2022-08-17 16:58:35 +0200

    fix shadow/background of the footer in the bo

--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="text-center text-secondary bg-light px-0">
+<footer id="footer" class="text-center text-secondary px-0">
     <div class="text-center p-2">
         <a href="https://github.com/alexis-gss/gamesgallery" target="_blank" data-bs="tooltip" data-bs-placement="top"
             title="{{ __('footer.website') }}" class="text-reset text-decoration-none">{{ $name }}</a>
