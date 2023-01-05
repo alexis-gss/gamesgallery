99c4346e125457711645897800f3647ce3dcd0b4
Alexis Gousseau <alexis.gousseau@ldc.fr>
2023-01-05 16:04:13 +0100

    update the console module message

--- resources/ts/modules/console.ts
+++ resources/ts/modules/console.ts
@@ -1,22 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     /**
-     * Show and style the title.
-     */
-    const consoleTitle = [
-        "font-weight: bold;",
-        "font-size: 50px;",
-        "color: rgb(255, 0, 0);",
-        "text-shadow: 3px 3px 0 rgb(0, 0, 0)",
-    ].join(";");
-    console.log("%cHey ‼", consoleTitle);
-
-    /**
      * Show and style the text.
      */
     const consoleText = [
         "font-weight: bold;",
-        "font-size: 18px;",
+        "font-size: 14px;",
         "color: rgb(255, 255, 255);",
     ].join(";");
-    console.log("%cI'm watching you...", consoleText);
+    console.log("%cMaybe there is something here... ?", consoleText);
 });
