43597e258ae0e10d13a4f23b17510aca4a5a352c
Alexis Gousseau <alexis.gousseau@ldc.fr>
2023-04-27 09:55:28 +0200

    update search bar back office

--- resources/lang/en/search.php
+++ resources/lang/en/search.php
@@ -1,10 +1,7 @@
 <?php
 
 return [
-    'search_game' => 'Search a game',
-    'search_folder' => 'Search a folder',
-    'search_user' => 'Search an user',
-    'search_tag' => 'Search a tag',
+    'search_words' => 'Your keywords here...',
     'apply_search' => 'Apply a search',
     'remove_search' => 'Remove the search'
 ];
--- resources/views/back/tags/index.blade.php
+++ resources/views/back/tags/index.blade.php
@@ -17,30 +17,7 @@
             </a>
         @endcan
     </div>
-    <form action="{{ request()->url() }}" enctype="multipart/form-data" id="search"
-        class="d-flex flex-row input-group pt-3 pb-2">
-        <label class="input-group-text" for="searchField">{{ $searchFields }}</label>
-        <input class="form-control"
-            type="text"
-            placeholder="{{ __('search.search_tag') }}"
-            id="searchField"
-            name="search"
-            value="{{ old('search', $search ?? '') }}">
-        <button class="btn btn-primary"
-            type="submit"
-            data-bs="tooltip"
-            data-bs-placement="top"
-            title="{{ __('search.apply_search') }}">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </button>
-        <a class="btn btn-danger"
-            data-bs="tooltip"
-            data-bs-placement="top"
-            title="{{ __('search.remove_search') }}"
-            href="{{ route('bo.tags.index') }}">
-            <i class="fa-solid fa-delete-left"></i>
-        </a>
-    </form>
+    @include('back.modules.search-bar')
     <table class="table">
         @if (count($tags) > 0)
             <thead>
