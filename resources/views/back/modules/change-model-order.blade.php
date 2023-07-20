c9f503b4ea80a70f4218139d857e11b32c1f32e9
Alexis Gousseau <alexis.gousseau@ldc.fr>
2023-07-20 14:51:46 +0200

    update order arrows and table-col-sorter

--- resources/views/back/modules/change-model-order.blade.php
+++ resources/views/back/modules/change-model-order.blade.php
@@ -1,6 +1,15 @@
 <td class="text-center align-middle">
     @can('isAdmin')
     <div class="d-flex justify-content-center align-items-center">
+        <a href="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'down']) }}"
+            class="@if($loop->last and $models->currentPage() === $models->lastPage()) invisible @endif">
+            <button type="submit"
+                class="btn btn-sm btn-outline-dark me-1 @if($loop->last and $models->currentPage() === $models->lastPage()) disabled @endif"
+                title="{{ __('list.down') }}"
+                data-bs="tooltip">
+                <i class="fas fa-arrow-down"></i>
+            </button>
+        </a>
         <a href="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'up']) }}"
             class="@if($loop->first and $models->onFirstPage()) invisible @endif">
             <button type="submit"
@@ -8,15 +17,6 @@
                 title="{{ __('list.up') }}"
                 data-bs="tooltip">
                 <i class="fas fa-arrow-up"></i>
-            </button>
-        </a>
-        <a href="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'down']) }}"
-            class="@if($loop->last and $models->currentPage() === $models->lastPage()) invisible @endif">
-            <button type="submit"
-                class="btn btn-sm btn-outline-dark ms-1 @if($loop->last and $models->currentPage() === $models->lastPage()) disabled @endif"
-                title="{{ __('list.down') }}"
-                data-bs="tooltip">
-                <i class="fas fa-arrow-down"></i>
             </button>
         </a>
     </div>
