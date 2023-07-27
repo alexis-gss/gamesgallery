d5c2101aab251a1897d8a0e63de78522c9694db2
Alexis Gousseau <alexis.gousseau@ldc.fr>
2023-07-27 13:01:28 +0200

    add statics in the back office - issue #23

---
+++ resources/views/back/statistics/chart-games-by-tags.blade.php
@@ -0,0 +1,77 @@
+<div class="charts w-100" id="chart-games-by-tags"></div>
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        /** chart-games-by-tags */
+        var chartGamesByTags = window.Echarts.init(document.getElementById('chart-games-by-tags'), 'royal');
+        var optionGamesByTags = {
+            title: {
+                text: 'Games by tag',
+                left: 'center',
+                textStyle: {
+                    fontSize: '16px',
+                    color: '#000',
+                },
+            },
+            tooltip: {
+                trigger: 'axis',
+                backgroundColor: 'rgba(15,15,15,0.90)',
+                borderColor: 'rgba(0,0,0,0)',
+                textStyle: {
+                    color: '#FFF',
+                },
+                formatter: (params) => {
+                    return `<strong>${params[0].name}</strong><br/>
+                        <span class="badge" style="background-color:${params[0].color}"></span>&nbsp;
+                        ${params[0].seriesName}&nbsp;:&nbsp;<strong>${params[0].value}</strong>&nbsp;(${Math.round((params[0].value/@json(count($globalGames))*100) * 100) / 100}%)`;
+                },
+            },
+            dataZoom: [
+                {
+                    xAxisIndex: 0,
+                }
+            ],
+            toolbox: {
+                show : true,
+                feature : {
+                    dataView : {show: true, readOnly: false},
+                    restore : {show: true},
+                    saveAsImage : {show: true},
+                }
+            },
+            calculable : true,
+            xAxis : [
+                {
+                    type : 'category',
+                    data : @json($globalTags->map(function ($tag) { return $tag->name; })),
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value',
+                }
+            ],
+            series : [
+                {
+                    name: 'Game',
+                    type: 'bar',
+                    data: @json($globalTags->map(function ($tag) { return count($tag->games); })),
+                    color: '#0D6EFD',
+                    markPoint : {
+                        data : [
+                            {type : 'max', name: 'Max'},
+                            {type : 'min', name: 'Min'},
+                        ]
+                    },
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        chartGamesByTags.setOption(optionGamesByTags);
+    });
+</script>
