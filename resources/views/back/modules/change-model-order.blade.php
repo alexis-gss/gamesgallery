2a125712210dc4394f6c6755207eb16497c90ba9
Alexis Gousseau <alexis.gousseau@ldc.fr>
2023-08-30 13:21:33 +0200

    update change-model-order request method

--- resources/views/back/modules/change-model-order.blade.php
+++ resources/views/back/modules/change-model-order.blade.php
@@ -1,24 +1,28 @@
 <td class="text-center align-middle">
     @can('isAdmin')
     <div class="d-flex justify-content-center align-items-center">
-        <a href="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'down']) }}"
+        <form action="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'down']) }}"
+            method="POST"
             class="@if($loop->last and $models->currentPage() === $models->lastPage()) invisible @endif">
+            @csrf
             <button type="submit"
                 class="btn btn-sm btn-outline-dark me-1 @if($loop->last and $models->currentPage() === $models->lastPage()) disabled @endif"
                 title="{{ __('crud.other.down') }}"
                 data-bs="tooltip">
                 <i class="fas fa-arrow-down"></i>
             </button>
-        </a>
-        <a href="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'up']) }}"
+        </form>
+        <form action="{{ route('bo.' . $routeName . '.change-order', [Str::singular($routeName) => $model, 'direction' => 'up']) }}"
+            method="POST"
             class="@if($loop->first and $models->onFirstPage()) invisible @endif">
+            @csrf
             <button type="submit"
                 class="btn btn-sm btn-outline-dark @if(($loop->first and $models->onFirstPage())) disabled @endif"
                 title="{{ __('crud.other.up') }}"
                 data-bs="tooltip">
                 <i class="fas fa-arrow-up"></i>
             </button>
-        </a>
+        </form>
     </div>
     @else
     @include('back.modules.user-right')
