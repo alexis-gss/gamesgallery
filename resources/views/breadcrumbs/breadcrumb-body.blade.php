8aa9485f4faa52f91a40ee252ba9bbc0bcf9dfd3
Alexis Gousseau <alexis.gousseau1@gmail.com>
2023-09-21 22:28:55 +0200

    update breadcrumbs back end

--- resources/views/breadcrumbs/breadcrumb-body.blade.php
+++ resources/views/breadcrumbs/breadcrumb-body.blade.php
@@ -1,12 +1,7 @@
 @hasSection('breadcrumb')
-@if(Breadcrumbs::exists(app()->view->getSections()['breadcrumb']))
-{{ call_user_func_array('Breadcrumbs::render', array_merge(
-    [app()->view->getSections()['breadcrumb']],
-    !empty($brParam) ? (is_array($brParam) ? $brParam : [$brParam]) : []
-)) }}
+@if (Breadcrumbs::exists(app()->view->getSections()['breadcrumb']))
+{{ call_user_func_array('Breadcrumbs::render', array_merge([app()->view->getSections()['breadcrumb']], Arr::wrap($brParam))) }}
 @endif
-@elseif(request()->route() != null)
-@if(Breadcrumbs::exists(request()->route()->getName()))
+@elseif(Breadcrumbs::exists(request()->route()->getName()))
 {{ Breadcrumbs::render(request()->route()->getName()) }}
-@endif
 @endif
