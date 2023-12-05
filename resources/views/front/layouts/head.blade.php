d8d6916e775797dbc9c74eddb1e31544b77aa0d8
Alexis Gousseau <alexis.gousseau1@gmail.com>
2023-12-05 14:49:49 +0100

    fix head blade ordering

--- resources/views/front/layouts/head.blade.php
+++ resources/views/front/layouts/head.blade.php
@@ -1,37 +1,46 @@
-<!-- Title -->
-<title>{{ config('app.name') }} - @yield('title')</title>
+{{-- Title --}}
+<title>{{ config('app.name') }}@hasSection('title') - @yield('title')@endif</title>
 
-<!-- Meta -->
-<meta charset="UTF-8" />
+{{-- Meta --}}
+<meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="csrf-token" content="{{ csrf_token() }}" />
-<meta name="description" content="@yield('description')" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="description" content="@yield('description')">
 <meta name="robots" content="index,follow">
 
-<!-- Open Graph -->
-<meta property="og:title" content="{{ config('app.name') }} - @yield('title')" />
+{{-- Open Graph --}}
+<meta property="og:title" content="{{ config('app.name') }}@hasSection('title') - @yield('title')@endif" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ config('app.url') }}" />
 <meta property="og:site_name" content="{{ config('app.name') }}" />
 <meta property="og:image" content="{{ asset('assets/images/visual-fo.png') }}" />
 <meta property="og:description" content="@yield('description')" />
 
-<!--  Favicon -->
-<link rel="icon" href="{{ asset('favicon.ico') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-<link rel="manifest" href="{{ asset('site.webmanifest') }}">
-<link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
+{{--  Favicon --}}
+<link href="{{ asset('favicon.ico') }}" rel="icon">
+<link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180">
+<link type="image/png" href="{{ asset('favicon-16x16.png') }}" rel="icon" sizes="16x16">
+<link type="image/png" href="{{ asset('favicon-32x32.png') }}" rel="icon" sizes="32x32">
+<link href="{{ asset('site.webmanifest') }}" rel="manifest">
+<link href="{{ asset('safari-pinned-tab.svg') }}" rel="mask-icon" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#2b5797">
 <meta name="msapplication-TileImage" content="{{ asset('mstile-144x144.png') }}">
 <meta name="theme-color" content="#121416">
 
-<!-- Others -->
-<link rel="canonical" href="{{ url()->current() }}" />
+{{-- Others --}}
+<link href="{{ \config('app.url') }}/{{ \request()->path() }}" rel="canonical" />
 @include('breadcrumbs.breadcrumb-head')
 
-<!-- Styles -->
+{{-- Styles --}}
 @vite(['resources/sass/fo/front.scss'])
+@stack('styles')
+
+{{-- DEBUG --}}
+@if (config('app.debug'))
+<script nonce="{{ $nonce }}">
+    window.vueDebug = true;
+</script>
+@endif
+
