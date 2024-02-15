b50076e7103d7d0c747080c798830e829d4fd733
Alexis Gousseau <alexis.gousseau1@gmail.com>
2024-02-15 09:35:22 +0100

    set throttle middleware by default

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            'throttle:40,1',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -39,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:40,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
