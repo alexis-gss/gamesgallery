59be2d4e54347b21d81c24fcf1167ef135b1145e
Alexis Gousseau <alexis.gousseau1@gmail.com>
2024-02-22 20:07:21 +0100

    open statistic picture in bootstrap modal

--- resources/views/back/pages/statistics/chart-pictures-by-ratings.blade.php
+++ resources/views/back/pages/statistics/chart-pictures-by-ratings.blade.php
@@ -8,15 +8,34 @@
 <ul class="list-group border-0">
     @foreach ($picturesRatings as $key => $pictureRating)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a class="btn btn-primary btn-sm" data-bs-tooltip="tooltip"
-                href="{{ sprintf('%s/storage/pictures/%s/%s.webp', config('app.url'), $pictureRating->picture->game->slug, $pictureRating->picture->uuid) }}"
-                title="{{ __('crud.actions_model.show', ['model' => __('models.picture')]) }}" target="_blank">
+            <button class="btn btn-primary btn-sm" data-bs-tooltip="tooltip" data-bs-toggle="modal" data-bs-target="#ModalViewPicture"
+                type="button" title="{{ __('crud.actions_model.show', ['model' => __('models.picture')]) }}">
                 {{ __('bo_other_stats_picture_id', [
                     'id' => $pictureRating->picture->id,
                     'game' => $pictureRating->picture->game->name,
                 ]) }}
-            </a>
+            </button>
             <span class="badge bg-secondary rounded-pill">{{ $pictureRating->ratings_count }}</span>
+            <div class="modal" id="ModalViewPicture" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" tabindex="-1">
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">
+                                    {{ Str::of(__('models.picture'))->ucFirst()->value() }}
+                                </h5>
+                                <button class="btn-close" data-bs-dismiss="modal" data-bs-tooltip="tooltip" type="button"
+                                    aria-label="{{ __('bo_other_close') }}" :title="__('bo_other_close')" />
+                            </div>
+                            <div class="modal-body">
+                                <img class="img-fluid"
+                                    src="{{ sprintf('%s/storage/pictures/%s/%s.webp', config('app.url'), $pictureRating->picture->game->slug, $pictureRating->picture->uuid) }}"
+                                    alt="{{ $pictureRating->picture->label }}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </li>
     @endforeach
 </ul>
